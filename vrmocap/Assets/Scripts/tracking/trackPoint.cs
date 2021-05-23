8b93db4004a1c37af89687d4f01be06bb63c3037
Albert Cho <albert.cho.kor@gmail.com>
2021-05-22 17:25:35 -0700

    Created simple script to track position and output to .txt file. Attach to desired object.

---
+++ vrmocap/Assets/Scripts/tracking/trackPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackPoint : MonoBehaviour
+{
+    private float timeElapsed;
+    private List<Vector3> positions = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= 1) {
+            positions.Add(transform.position);
+            timeElapsed = 0;
+        }
+    }
+
+    void OnDestroy()
+    {
+        string outputString = "";
+        foreach(Vector3 position in positions){
+            //three decimal places
+            outputString += position.ToString("F3");
+        }
+
+        System.IO.File.WriteAllText(@"C:\Users\alber\Downloads\data.txt", outputString);
+    }
+}
